ab79ef2328e0e72bd4e729ea264c1977e0ff2a3e
Tevin Davis <tevindavis@icloud.com>
2023-05-19 16:49:55 -0400

    Module programming completed

--- Exercise/Exercise  Modular Programming/index.js
+++ Exercise/Exercise  Modular Programming/index.js
@@ -1,5 +1,8 @@
-/* 
-    1. Create an index.js file which imports login module and invokes validate method of User class.
+/*
+    1. Create an index.js file which imports login module and invokes validate method
     2. On submit of the button in HTML the validate method of the User class should be invoked.
     3. Implement the validate method to send the username and password details entered by the user and capture the return value to display in the alert.
 */
+
+import { validation } from './login.js'
+let validate = validation()
--- Exercise/Exercise  Modular Programming/login.js
+++ Exercise/Exercise  Modular Programming/login.js
@@ -1,5 +1,15 @@
-/* 
-1. Create a validate method with username and password as arguments.
-2. If the username and password are equal it will return "Login Successful" 
-    else will return "Unauthorized access".
-*/
+
+// Create a validate method with username and password as arguments.
+function validation () {
+    // If the username and password are equal it will return "Login Successful"
+    // else will return "Unauthorized access".
+    let password = document.getElementsByName('password').value
+    let username = document.getElementsByName('name').value
+    if (username === password) {
+        return alert('Login Successful')
+    } else {
+        return alert('Unauthorized access')
+    }
+}
+
+export { validation }
