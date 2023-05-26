ffb43ba760dafd8691b2f59feda3677904bba393
Tevin Davis <tevindavis@icloud.com>
2023-05-26 11:39:32 -0400

    working on main.js

--- Capstone Project/automobmechanic-app/js/main.js
+++ Capstone Project/automobmechanic-app/js/main.js
@@ -1,56 +1,89 @@
-// Add JavaScript Code here
+// Assign json to object
 
-// fetch username and password from json file
+let users;
 
-async function loadCredent() {
-    const requestURL = './users.json';
-    const request = new Request(requestURL);
-    const response = await fetch(request);
-    const nameText = await response.json();
-    const passText = await response.json();
+// fetch name, username, and password from json file
 
-    const names = JSON.parse(nameText);
-    const pass = JSON.parse(passText);
+fetch('/Capstone Project/automobmechanic-app/js/users.json')
+    .then(response => response.json())
+    .then(data => {
+        users = data
+        console.log(users)
+        validateUserInput()
+    })
+    .catch(error => {
+        console.log('An error occured:', error);
+    });
 
+// Object to hold user input
 
-    // Set username and password to value of html username and password field
-
-    function checkCredent () {
-        const username = document.getElementById('uname').value
-        const password = document.getElementById('pwd').value
-        const uname = JSON.strigfy(username)
-        const pwd = JSON.stringfy(password)
-
-        // check if username input match usernames in json file
-
-        if (uname === names && pwd === pass) {
-            alert('Logged in successfully');
-            location.assign('../html/home.html');
-
-        } else {
-            alert('Invalid credentials');
-        }
-
-        // Alter Dom navbar based on user role
-
-        switch () {
-        case uname === 'admin':
-            const service = document.getElementById('service')
-            const booking = document.getElementById('booking');
-            const login = document.getElementById('login');
-            const reporting = document.getElementById('reports');
-            service.remove();
-            booking.remove();
-            login.remove();
-            document.getElementById('name').innerHTML = 'Hi ' + object.uname;
-            break;
-        case uname === names:
-            reporting.remove();
-            document.getElementById('name').innerHTML = 'Hi ' + object.uname;
-            break;
-        default:
-            alert('You are not an authorized user!')
-            break;
-        }
-    }  
+const formData = {
+    username: '',
+    password: ''
 }
+
+// Function to store username and password in formData Object
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const usernameInput = document.getElementById('uname').value;
+    const passwordInput = document.getElementById('pwd').value;
+
+    formData.username = usernameInput;
+    formData.password = passwordInput;
+}
+
+// Add event listener to form
+
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    handleSubmit()
+});
+
+// Function to validate if user input match username/password in json file
+
+function validateUserInput() {
+
+    // Varables for DOM based on User role
+    const service = document.getElementById('service');
+    const booking = document.getElementById('booking');
+    const reporting = document.getElementById('reports');
+
+
+    if (users) {
+        switch (true) {
+            // Checks if user role is admin
+            case formData.username === users[0].username && formData.password === users[0].password:
+                sessionStorage.setItem('username', formData.username);
+                sessionStorage.setItem('password', formData.password);
+                alert('Logged in successfully');
+                location.assign('../html/home.html');
+                // Removes following elements from navbar on homepage
+                service.remove();
+                booking.remove();
+                break;
+            case formData.username === users.username && formData.password === users.password:
+                // checks for all other users in json file
+                sessionStorage.setItem('username', formData.username);
+                sessionStorage.setItem('password', formData.password);
+                alert('Logged in successfully');
+                location.assign('../html/home.html');
+                // Removes following elements from navbar on homepage
+                reporting.remove();
+                break;
+            default:
+                // If the userinput does not match any users in json file
+                alert('Invalid credentials: Please Try Again');
+        }
+    }
+}
+
+// add event listener to form submit button to validate user
+
+document.getElementById('btn').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    validateUserInput();
+})
