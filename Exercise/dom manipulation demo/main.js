678a2c8a0934348af0998dbac6a7c3758fef390b
Tevin Davis <tevindavis@icloud.com>
2023-05-30 14:45:48 -0400

    added a couple example

--- Exercise/dom manipulation demo/main.js
+++ Exercise/dom manipulation demo/main.js
@@ -1,7 +1,23 @@
+
+// In order to see how each example works comment out the section of code you do not what to use
+// Change Element Example Using Button
+
+
 function changeElement() {
     // Get user input from the input field
     const userInput = document.getElementById('inputField').value;
 
     // Change the text of the output element
-    document.getElementById('outputElement').innerHTML = userInput;
+    document.getElementById('outputElement').innerHTML = 'Hi ' + userInput;
 }
+
+
+// Change Element Example Without using button
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("inputField2").addEventListener("input", function() {
+        document.getElementById("outputElement2").innerHTML = this.value;
+    });
+
+});
+*/
